0ed8ffe885546a83eeea12dd068dac633391e4f9
Pete Dragun <pdragun@13bytes.com>
2024-03-09 17:15:15 -0500

    Add project files.

---
+++ DateFormatService.Prototype/DateTimeParserService.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace DateFormatService.Prototype;
+
+public interface IDateTimeParserService
+{
+    DateTime Parse(string value);
+}
+
+public class DateTimeParserService : IDateTimeParserService
+{
+    private List<string> _dateTimeFormatPatterns;
+
+    public DateTimeParserService(List<string> dateTimeFormatPatterns)
+    {
+        _dateTimeFormatPatterns = dateTimeFormatPatterns;
+    }
+
+    public DateTime Parse(string value)
+    {
+        foreach (var pattern in _dateTimeFormatPatterns)
+        {
+            if (DateTime.TryParseExact(value, pattern, null, DateTimeStyles.None, out var dateTimeFormat))
+            {
+                _dateTimeFormatPatterns.Remove(pattern);
+                _dateTimeFormatPatterns.Insert(0, pattern);
+                return dateTimeFormat;
+            }
+        }
+
+        throw new Exception("Unable to parse the string as a valid datetime");
+    }
+}
---
+++ DateFormatService.Prototype/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DateFormatService.Prototype;
+using System.Globalization;
+
+var formats = new List<string>
+{
+    "dd/MM/yyyy",    
+    "MM/dd/yyyy"
+};
+
+
+formats.AddRange(CultureInfo.CurrentCulture.DateTimeFormat.GetAllDateTimePatterns());
+
+foreach (var format in formats)
+{
+    Console.WriteLine(format);
+}
+
+var parser = new DateTimeParserService(formats);
+
+var listOfDates = new List<string>()
+{
+    "2024-02-02",
+    "2024-01-01",
+    "30/11/2023"
+};
+
+try
+{
+    foreach (var input in listOfDates)
+    {
+        DateTime parsedDateTime = parser.Parse(input);
+        Console.WriteLine("Successfully parsed date: " + parsedDateTime);
+    }
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
